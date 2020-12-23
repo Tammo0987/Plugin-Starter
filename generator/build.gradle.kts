888935c07e957398fb6ce3ae96015848941ede8d
Tammo <tammo.steffens@googlemail.com>
2020-12-23 20:47:41 +0100

    Created sub module for the generator

---
+++ generator/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "com.github.plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("org.redundent:kotlin-xml-builder:1.7.2")
+    implementation("com.squareup:kotlinpoet:1.7.2")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.0")
+
+    implementation("com.hendraanggrian:javapoet-ktx:0.8")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "Plugin-Starter"
+
+include("generator")
